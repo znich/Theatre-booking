3e3e460527b7a740de28de5b01897ecbdd78759a
Anatoly Lahuta <ptah-volya@mail.ru>
2013-05-31 22:30:31 +0300

    dao

--- dao/src/main/java/by/academy/DAO/category/ORACLECategoryDAO.java
+++ dao/src/main/java/by/academy/DAO/category/ORACLECategoryDAO.java
@@ -127,8 +127,6 @@ public class ORACLECategoryDAO implements CategoryDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	//test comment
-    	// sdgsdgdgsd
-    	//fgdgdfgd
+    	
     }
 }
